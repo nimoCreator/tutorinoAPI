eafbb1b3c92db4414960fbe25ad422d11596d32f
nimoCreator <sebastianlegierski2001@gmail.com>
2024-01-29 18:43:48 +0100

    More tests added
    Tests for all complex methods added
    
    Co-Authored-By: Artmansky <79310772+Artmansky@users.noreply.github.com>

--- APITests.py
+++ APITests.py
@@ -53,5 +53,39 @@ class TestGetMyOffersAPI(unittest.TestCase):
             self.assertIn("error", data)
             self.assertEqual(data["error"], "Login or email already exists in the database")
 
+    def test_validate_session(self):
+        session_id = "00000"
+
+        response = requests.get(f"http://173.212.216.164:5555/validateSession?sessionID={session_id}")
+
+        self.assertIn(response.status_code, [200, 409])
+
+        data = response.json()
+        if response.status_code == 200:
+            self.assertIn("message", data)
+            self.assertEqual(data["message"], "session valid")
+        elif response.status_code == 409:
+            self.assertIn("error", data)
+            self.assertEqual(data["error"], "Session ID wrong or expired")
+
+    def test_session_add_success(self):
+        data = {"userID": "123"}
+
+        response = self.app.post("http://173.212.216.164:5555/addSession", json=data)
+
+        json_data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("sessionId", json_data)
+
+    def test_session_add_failure(self):
+        data = {"userID": "invalid_user"}
+
+        response = self.app.post("http://173.212.216.164:5555/addSession", json=data)
+
+        json_data = response.get_json()
+        self.assertEqual(response.status_code, 409)
+        self.assertIn("error", json_data)
+        self.assertEqual(json_data["error"], "Failed to add new session")
+
 if __name__ == '__main__':
     unittest.main()
