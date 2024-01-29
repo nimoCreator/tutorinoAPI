00d0fb286a898e7c41c0e626e6fcdcb5cb0f63cb
nimoCreator <sebastianlegierski2001@gmail.com>
2024-01-29 18:31:22 +0100

    Proc File Deleted

--- APITests.py
+++ APITests.py
@@ -33,6 +33,25 @@ class TestGetMyOffersAPI(unittest.TestCase):
         elif response.status_code == 401:
             self.assertIn("detail", data) 
             self.assertEqual(data["detail"], "Invalid login or email")
-            
+
+    def test_register(self):
+        login = "user"
+        email = "newuser@gmail.com"
+        password = "password"
+
+        data = {"login": login, "email": email, "password": password}
+
+        response = requests.post("http://173.212.216.164:5555/register", json=data)
+
+        self.assertIn(response.status_code, [200, 409])
+
+        data = response.json()
+        if response.status_code == 200:
+            self.assertIn("access_token", data)
+            self.assertEqual(data["token_type"], "bearer")
+        elif response.status_code == 409:
+            self.assertIn("error", data)
+            self.assertEqual(data["error"], "Login or email already exists in the database")
+
 if __name__ == '__main__':
     unittest.main()
