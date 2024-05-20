159936db6b3890181eb8b9ce083f5cde9b895bf4
nimoCreator <sebastianlegierski2001@gmail.com>
2024-05-20 22:42:08 +0200

    Nowe API dodane
    Co-Authored-By: Artmansky <79310772+Artmansky@users.noreply.github.com>

--- TutorinoAPICS/TutorinoAPICS/Controllers/OffersController.cs
+++ TutorinoAPICS/TutorinoAPICS/Controllers/OffersController.cs
@@ -21,7 +21,7 @@ namespace TutorinoAPICS.Controllers
         //In progress, Code 100 - No offers
         [HttpGet]
         [Route("getAllOffers")]
-        public String GetUsers()
+        public String GetOffers()
         {
             SqlConnection con = new SqlConnection(configuration.GetConnectionString("AppCon").ToString());
             SqlDataAdapter data = new SqlDataAdapter("Select * from offers", con);
@@ -35,7 +35,7 @@ namespace TutorinoAPICS.Controllers
                     Offer ofr = new Offer();
                     ofr.id = Convert.ToInt32(dataTable.Rows[i]["oid"]);
                     ofr.price = Convert.ToDouble(dataTable.Rows[i]["price"]);
-                    ofr.description = Convert.ToString(dataTable.Rows[i]["[desc]"]);
+                    ofr.description = Convert.ToString(dataTable.Rows[i]["desc"]);
                     offers.Add(ofr);
                 }
             }
@@ -52,7 +52,7 @@ namespace TutorinoAPICS.Controllers
         //Codes 0 - Data added successfully, 100 - Other Error, 101 - User or Subject are not correct
         [HttpPost]
         [Route("addOffer")]
-        public String AddUser(OfferAdded newOffer)
+        public String AddOffer(OfferAdded newOffer)
         {
             SqlConnection con = new SqlConnection(configuration.GetConnectionString("AppCon").ToString());
             SqlCommand cmd = new SqlCommand("Insert into offers(kuid,sid,price,[desc]) values('" + newOffer.userID + "','" + newOffer.subjectID + "','" + newOffer.price + "','" + newOffer.description + "')", con);
@@ -73,6 +73,207 @@ namespace TutorinoAPICS.Controllers
             else
             {
                 return JsonConvert.SerializeObject(new Response());
+            }
+        }
+
+        [HttpPost]
+        [Route("editOffer")]
+        public String EditOffer(Offer offer)
+        {
+            using (SqlConnection con = new SqlConnection(configuration.GetConnectionString("AppCon")))
+            {
+                string query = "UPDATE offers SET kuid = @userID, sid = @subjectID, price = @price, [desc] = @description WHERE oid = @id";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@id", offer.id);
+                    cmd.Parameters.AddWithValue("@userID", offer.userID);
+                    cmd.Parameters.AddWithValue("@subjectID", offer.subjectID);
+                    cmd.Parameters.AddWithValue("@price", offer.price);
+                    cmd.Parameters.AddWithValue("@description", offer.description);
+
+                    con.Open();
+                    try
+                    {
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return JsonConvert.SerializeObject(new Response(0, "Offer updated succesfully"));
+                        }
+                        else
+                        {
+                            return JsonConvert.SerializeObject(new Response(100, "Offer not found"));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return JsonConvert.SerializeObject(new Response(101, "Error while updating - connection/serialization related: " + ex));
+                    }
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("deleteOffer")]
+        public String DeleteOffer(OfferIDD offer)
+        {
+            using (SqlConnection con = new SqlConnection(configuration.GetConnectionString("AppCon")))
+            {
+                string query = "DELETE FROM offers WHERE oid = @id";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@id", offer.offerID);
+
+                    con.Open();
+                    try
+                    {
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return JsonConvert.SerializeObject(new Response(0, "Offer deleted successfully"));
+                        }
+                        else
+                        {
+                            return JsonConvert.SerializeObject(new Response(100, "Offer not found"));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return JsonConvert.SerializeObject(new Response(101, "Error deleting offer"));
+                    }
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("getOffersByUserId")]
+        public string GetOffersByUserId(OfferUID offer)
+        {
+            using (SqlConnection con = new SqlConnection(configuration.GetConnectionString("AppCon")))
+            {
+                string query = "SELECT * FROM offers WHERE kuid = @userId";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@userId", offer.userID);
+
+                    SqlDataAdapter data = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    data.Fill(dataTable);
+                    List<Offer> offers = new List<Offer>();
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            Offer ofr = new Offer
+                            {
+                                id = Convert.ToInt32(row["oid"]),
+                                userID = Convert.ToInt32(row["kuid"]),
+                                subjectID = Convert.ToInt32(row["sid"]),
+                                price = Convert.ToDouble(row["price"]),
+                                description = Convert.ToString(row["desc"])
+                            };
+                            offers.Add(ofr);
+                        }
+                    }
+
+                    if (offers.Count > 0)
+                    {
+                        return JsonConvert.SerializeObject(offers);
+                    }
+                    else
+                    {
+                        return JsonConvert.SerializeObject(new Response(100, "Offer not found for the specified user ID"));
+                    }
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("getOffersBySubjectId")]
+        public string GetOffersBySubjectId(OfferSubject subject)
+        {
+            using (SqlConnection con = new SqlConnection(configuration.GetConnectionString("AppCon")))
+            {
+                string query = "SELECT * FROM offers WHERE sid = @subjectId";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@subjectId", subject.subjectID);
+
+                    SqlDataAdapter data = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    data.Fill(dataTable);
+                    List<Offer> offers = new List<Offer>();
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            Offer ofr = new Offer
+                            {
+                                id = Convert.ToInt32(row["oid"]),
+                                userID = Convert.ToInt32(row["kuid"]),
+                                subjectID = Convert.ToInt32(row["sid"]),
+                                price = Convert.ToDouble(row["price"]),
+                                description = Convert.ToString(row["desc"])
+                            };
+                            offers.Add(ofr);
+                        }
+                    }
+
+                    if (offers.Count > 0)
+                    {
+                        return JsonConvert.SerializeObject(offers);
+                    }
+                    else
+                    {
+                        return JsonConvert.SerializeObject(new Response(100, "Offer not found for the specified subject ID"));
+                    }
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("getOffersByUIdAndSId")]
+        public string GetOffersByUIdAndSId(OfferID offer)
+        {
+            using (SqlConnection con = new SqlConnection(configuration.GetConnectionString("AppCon")))
+            {
+                string query = "SELECT * FROM offers WHERE kuid = @userId AND sid = @subjectId";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@userId", offer.userID);
+                    cmd.Parameters.AddWithValue("@subjectId", offer.subjectID);
+
+                    SqlDataAdapter data = new SqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    data.Fill(dataTable);
+                    List<Offer> offers = new List<Offer>();
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            Offer ofr = new Offer
+                            {
+                                id = Convert.ToInt32(row["oid"]),
+                                userID = Convert.ToInt32(row["kuid"]),
+                                subjectID = Convert.ToInt32(row["sid"]),
+                                price = Convert.ToDouble(row["price"]),
+                                description = Convert.ToString(row["desc"])
+                            };
+                            offers.Add(ofr);
+                        }
+                    }
+
+                    if (offers.Count > 0)
+                    {
+                        return JsonConvert.SerializeObject(offers);
+                    }
+                    else
+                    {
+                        return JsonConvert.SerializeObject(new Response(100, "No offers found for the specified user ID and subject ID"));
+                    }
+                }
             }
         }
     }
--- TutorinoAPICS/TutorinoAPICS/Models/Offer.cs
+++ TutorinoAPICS/TutorinoAPICS/Models/Offer.cs
@@ -3,9 +3,10 @@
     public class Offer
     {
         public int id { get; set; }
-        public int userID {  get; set; }
+        public int userID { get; set; }
+        public int subjectID { get; set; }
         public double price { get; set; }
-        public string description {  get; set; }
+        public string description { get; set; }
     }
 
     public class OfferAdded
@@ -14,5 +15,25 @@
         public int subjectID { get; set; }
         public double price { get; set; }
         public string description { get; set; }
+    }
+
+    public class OfferSubject
+    {
+        public int subjectID { get; set; }
+    }
+
+    public class OfferID
+    {
+        public int subjectID { set; get; }
+        public int userID { set; get; }
+    }
+
+    public class OfferUID
+    {
+        public int userID { set; get; }
+    }
+
+    public class OfferIDD {
+        public int offerID { get; set; }
     }
 }
