18e9be6b257df3ae3ccc115491648f744e62a9d6
nimoCreator <sebastianlegierski2001@gmail.com>
2024-06-04 21:16:39 +0200

    Get all corepe
    Co-Authored-By: Artmansky <79310772+Artmansky@users.noreply.github.com>

--- TutorinoAPICS/TutorinoAPICS/Controllers/CorepetitionsController.cs
+++ TutorinoAPICS/TutorinoAPICS/Controllers/CorepetitionsController.cs
@@ -19,6 +19,50 @@ namespace TutorinoAPICS.Controllers
 
     }
 
-    
+    [HttpGet]
+    [Route("getTrans")]
+    public String getTrans(){
+        SqlConnection con = new SqlConnection(configuration.GetConnectionString("AppCon").ToString());
+        SqlDataAdapter data = new SqlDataAdapter("Select * from corepetitions", con);
+        DataTable dataTable = new DataTable();
+        data.Fill(dataTable);
+        List<Corepetitions> list = new List<Corepetitions>();
+        if (dataTable.Rows.Count > 0)
+        {
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                Corepetitions core = new Corepetitions();
+                core.zuid = Convert.ToInt32(dataTable.Rows[i]["zuid"]);
+                core.teacher = Convert.ToInt32(dataTable.Rows[i]["teacher"]);
+                core.pupil = Convert.ToInt32(dataTable.Rows[i]["pupil"]);
+                core.subject = Convert.ToInt32(dataTable.Rows[i]["subject"]);
+                core.level = Convert.ToInt32(dataTable.Rows[i]["level"]);
+                core.status = Convert.ToString(dataTable.Rows[i]["status"]);
+                core.start = Convert.ToDateTime(dataTable.Rows[i]["start"]);
+                core.end = Convert.ToDateTime(dataTable.Rows[i]["end"]);
+                core.time = Convert.ToInt32(dataTable.Rows[i]["time"]);
+                core.price = Convert.ToDouble(dataTable.Rows[i]["price"]);
+                core.currency = Convert.ToString(dataTable.Rows[i]["currency"]);
+                core.form = Convert.ToChar(dataTable.Rows[i]["form"]);
+                core.meet_link = Convert.ToString(dataTable.Rows[i]["meet_link"]);
+                core.table_link = Convert.ToString(dataTable.Rows[i]["table_link"]);
+                core.localization = Convert.ToString(dataTable.Rows[i]["localization"]);
+                core.accepted_o = Convert.ToBool(dataTable.Rows[i]["accepted_o"]);
+                core.accepted_k = Convert.ToBool(dataTable.Rows[i]["accepted_k"]);
+                core.paid_in_cash = Convert.ToBool(dataTable.Rows[i]["paid_in_cash"]);
+                core.trainsaction_id = Convert.ToInt32(dataTable.Rows[i]["trainsaction_id"]);
+                core.convo = Convert.ToInt32(dataTable.Rows[i]["convo"]);
+                list.Add(t);
+            }
+        }
+        if(list.Count>0)
+        {
+            return JsonConvert.SerializeObject(list);
+        }
+        else
+        {
+            return JsonConvert.SerializeObject(new Response());
+        }
+    }
 
 }
